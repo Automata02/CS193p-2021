623622255bff4754eed0fdecc13b3bc263672fb8
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2023-02-09 16:14:10 +0200

    Completed Lecture 8
    Refactored the project files as they are meant for following along the lectures. Now it reflects only the progress done in Lectures not Assignments. Assignments moved to separate folder.

---
+++ Lecture 8/Memorize/Memorize/EmojiMemoryGame+ViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiMemoryGame+ViewModel.swift
+//  Memorize
+//
+//  Created by roberts.kursitis on 31/01/2023.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+	typealias Card = MemoryGame<String>.Card
+	
+	private static let emojis = ["✈️", "🚗", "🚁", "🚝", "🚀", "🚋", "🚕", "🚲", "🚌", "⛴️", "🚃"]
+
+	private static func createMemoryGame() -> MemoryGame<String> {
+		MemoryGame<String>(numberOfPairsOfCards: 8) { pairIndex in
+			emojis[pairIndex]
+		}
+	}
+	
+	@Published private var model = createMemoryGame()
+	
+	var cards: Array<Card> {
+		model.cards
+	}
+	
+	// MARK: - Intent(s)
+	
+	func choose(_ card: Card) {
+		model.choose(card)
+	}
+	
+	func shuffle() {
+		model.shuffle()
+	}
+	
+	func restart() {
+		model = EmojiMemoryGame.createMemoryGame()
+	}
+}
+
--- Lecture 8/Memorize/Memorize/MemoryGame+Model.swift
+++ Lecture 8/Memorize/Memorize/MemoryGame+Model.swift
@@ -24,31 +24,18 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
 	mutating func choose(_ card: Card) {
 		if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
 		   !cards[chosenIndex].isFaceUp,
-		   !cards[chosenIndex].isMatched {
+		   !cards[chosenIndex].isMatched
+		{
 			if let potentialMatchIndex = indexOfOnlyFaceUpCard {
 				if cards[chosenIndex].content == cards[potentialMatchIndex].content {
 					cards[chosenIndex].isMatched = true
 					cards[potentialMatchIndex].isMatched = true
-					score += 2
-				} else {
-					if seenCardArray.contains(chosenIndex) {
-						score -= 1
-					} else {
-						seenCardArray.append(chosenIndex)
-					}
-					if seenCardArray.contains(potentialMatchIndex) {
-						score -= 1
-					} else {
-						seenCardArray.append(potentialMatchIndex)
-					}
 				}
 				cards[chosenIndex].isFaceUp = true
 			} else {
 				indexOfOnlyFaceUpCard = chosenIndex
 			}
 		}
-		
-		print("\(cards)")
 	}
 	
 	mutating func shuffle() {
@@ -57,32 +44,77 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
 	
 	init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
 		cards = []
-		//add numberOfPairsOfCards x 2 cards to cards array
 		for pairIndex in 0..<numberOfPairsOfCards {
 			let content = createCardContent(pairIndex)
-			cards.append(Card(content: content, id: pairIndex*2))
-			cards.append(Card(content: content, id: pairIndex*2+1))
+			cards.append(Card(content: content, id: pairIndex * 2))
+			cards.append(Card(content: content, id: pairIndex * 2 + 1))
 		}
-		
-//		cards = cards.shuffled()
 		cards.shuffle()
-		score = 0
 	}
 	
 	struct Card: Identifiable {
-		var isFaceUp = false
-		var isMatched = false
+		var isFaceUp = false {
+			didSet {
+				if isFaceUp {
+					startUsingBonusTime()
+				} else {
+					stopUsingBonusTime()
+				}
+			}
+		}
+		var isMatched = false {
+			didSet {
+				stopUsingBonusTime()
+			}
+		}
 		let content: CardContent
 		let id: Int
+		
+		var bonusTimeLimit: TimeInterval = 6
+		
+		private var faceUpTime: TimeInterval {
+			if let lastFaceUpDate = self.lastFaceUpDate {
+				return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+			} else {
+				return pastFaceUpTime
+			}
+		}
+		
+		var lastFaceUpDate: Date?
+		
+		var pastFaceUpTime: TimeInterval = 0
+		
+		var bonusTimeRemaining: TimeInterval {
+			max(0, bonusTimeLimit - faceUpTime)
+		}
+		
+		var bonusRemaining: Double {
+			(bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit: 0
+		}
+		
+		var hasEarnedBonus: Bool {
+			isMatched && bonusTimeRemaining > 0
+		}
+		
+		var isConsumingBonusTime: Bool {
+			isFaceUp && !isMatched && bonusTimeRemaining > 0
+		}
+		
+		private mutating func startUsingBonusTime() {
+			if isConsumingBonusTime, lastFaceUpDate == nil {
+				lastFaceUpDate = Date()
+			}
+		}
+		
+		private mutating func stopUsingBonusTime() {
+			pastFaceUpTime = faceUpTime
+			self.lastFaceUpDate = nil
+		}
 	}
 }
 
 extension Array {
 	var oneAndOnly: Element? {
-		if self.count == 1 {
-			return self.first
-		} else {
-			return nil
-		}
+		return count == 1 ? first : nil
 	}
 }
