cd8b4ac40b35608c14d257ba3db536821d579e58
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2023-02-09 11:12:17 +0200

    Lecture 7 update

---
+++ Lecture 7/Memorize/Memorize/Cardify.swift
@@ -0,0 +1,40 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by roberts.kursitis on 09/02/2023.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+	var isFaceUp: Bool
+	
+	func body(content: Content) -> some View {
+		ZStack {
+			let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+	
+			if isFaceUp {
+				shape.fill().foregroundColor(.white)
+				shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+			} else {
+				shape.fill()
+			}
+			content
+				.opacity(isFaceUp ? 1 : 0)
+		}
+		.padding(EdgeInsets(top: 3, leading: 0, bottom: 3, trailing: 0))
+	}
+	
+	private struct DrawingConstants {
+		static let cornerRadius: CGFloat = 10
+		static let lineWidth: CGFloat = 3
+		static let fontScale: CGFloat = 0.65
+	}
+}
+
+extension View {
+	func cardify(isFaceUp: Bool) -> some View {
+		self.modifier(Cardify(isFaceUp: isFaceUp))
+	}
+}
--- Lecture 7/Memorize/Memorize/EmojiMemoryGameView.swift
+++ Lecture 7/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -25,49 +25,49 @@ struct EmojiMemoryGameView: View {
 							}
 					}
 				}
-				if isShowingSettings {
-					VStack {
-						Text("Pick a theme!")
-							.font(.title)
-						Picker("", selection: $game.selectedTheme) {
-							ForEach(EmojiMemoryGame.Themes.allCases, id: \.self) { theme in
-								Label(theme.rawValue.capitalized, systemImage: EmojiMemoryGame.titles[theme.rawValue] ?? "")
-							}
-						}
-						HStack {
-							ForEach(EmojiMemoryGame.colors, id: \.self) { color in
-								Image(systemName: "circle.fill")
-									.foregroundColor(color)
-									.onTapGesture {
-										game.changeThemeColor(color)
-									}
-							}
-						}
-						.font(.largeTitle)
-						.padding()
-					}
-				} else {
-					Text("Score: \(game.fetchScore())")
-				}
-			}
-			.navigationTitle(game.currentTheme.title.capitalized)
-			.toolbar {
-				ToolbarItem(placement: .navigationBarLeading) {
-					Button {
-						game.restartGame()
-					} label: {
-						Image(systemName: "arrow.clockwise.circle.fill")
-							.foregroundColor(.primary)
-					}
-				}
-				ToolbarItem(placement: .navigationBarTrailing) {
-					Button {
-						isShowingSettings.toggle()
-					} label: {
-						Image(systemName: "gearshape.fill")
-							.foregroundColor(.primary)
-					}
-				}
+//				if isShowingSettings {
+//					VStack {
+//						Text("Pick a theme!")
+//							.font(.title)
+//						Picker("", selection: $game.selectedTheme) {
+//							ForEach(EmojiMemoryGame.Themes.allCases, id: \.self) { theme in
+//								Label(theme.rawValue.capitalized, systemImage: EmojiMemoryGame.titles[theme.rawValue] ?? "")
+//							}
+//						}
+//						HStack {
+//							ForEach(EmojiMemoryGame.colors, id: \.self) { color in
+//								Image(systemName: "circle.fill")
+//									.foregroundColor(color)
+//									.onTapGesture {
+//										game.changeThemeColor(color)
+//									}
+//							}
+//						}
+//						.font(.largeTitle)
+//						.padding()
+//					}
+//				} else {
+//					Text("Score: \(game.fetchScore())")
+//				}
+//			}
+//			.navigationTitle(game.currentTheme.title.capitalized)
+//			.toolbar {
+//				ToolbarItem(placement: .navigationBarLeading) {
+//					Button {
+//						game.restartGame()
+//					} label: {
+//						Image(systemName: "arrow.clockwise.circle.fill")
+//							.foregroundColor(.primary)
+//					}
+//				}
+//				ToolbarItem(placement: .navigationBarTrailing) {
+//					Button {
+//						isShowingSettings.toggle()
+//					} label: {
+//						Image(systemName: "gearshape.fill")
+//							.foregroundColor(.primary)
+//					}
+//				}
 			}
 		}
 	}
@@ -76,7 +76,7 @@ struct EmojiMemoryGameView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
 		let game = EmojiMemoryGame()
-		game.choose(game.cards.first!)
+//		game.choose(game.cards.first!)
 		return EmojiMemoryGameView(game: game)
     }
 }
@@ -93,21 +93,19 @@ struct CardView: View {
 	var body: some View {
 		GeometryReader { geometry in
 			ZStack {
-				let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-				
-				if card.isFaceUp {
-					shape.fill().foregroundColor(.white)
-					shape.strokeBorder(lineWidth: DrawingConstants.lineWidth).foregroundColor(color)
-					Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(5).opacity(0.5)
-					Text(card.content).font(font(in: geometry.size))
-				} else if card.isMatched {
-					shape.opacity(0)
-				} else {
-					shape.fill().foregroundColor(color)
-				}
+				Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(5).opacity(0.5)
+				Text(card.content)
+					.animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: false))
+					.rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
+					.font(Font.system(size: DrawingConstants.fontSize))
+					.scaleEffect(scale(thatFits: geometry.size))
 			}
-			.padding(EdgeInsets(top: 3, leading: 0, bottom: 3, trailing: 0))
+			.cardify(isFaceUp: card.isFaceUp)
 		}
+	}
+	
+	private func scale(thatFits size: CGSize) -> CGFloat {
+		min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
 	}
 	
 	private func font(in size: CGSize) -> Font {
@@ -115,8 +113,7 @@ struct CardView: View {
 	}
 	
 	private struct DrawingConstants {
-		static let cornerRadius: CGFloat = 10
-		static let lineWidth: CGFloat = 3
+		static let fontSize: CGFloat = 32
 		static let fontScale: CGFloat = 0.65
 	}
 }
